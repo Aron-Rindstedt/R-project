7b544d8ec9ba393cc3cc4369ccc818c159dc674d
maxblock <mblock@live.se>
2015-08-03 11:34:22 +0200

    Minor updates to filnames etc

--- R-files/shelter.R
+++ R-files/shelter.R
@@ -4,9 +4,9 @@ run <- function(){
     shelter.reduced.data <- reduce(coef)
     
     shelter.list <- divide(shelter.reduced.data)
-    assign("shelter.train", shelter.list[[1]], envir = .GlobalEnv)
-    assign("shelter.val", shelter.list[[2]], envir = .GlobalEnv)
-    assign("shelter.test", shelter.list[[3]], envir = .GlobalEnv)
+    assign("n.items.train", shelter.list[[1]], envir = .GlobalEnv)
+    assign("n.items.val", shelter.list[[2]], envir = .GlobalEnv)
+    assign("n.items.test", shelter.list[[3]], envir = .GlobalEnv)
 
     model <- model()
     return(evaluate(model))
@@ -29,20 +29,30 @@ init <- function() {
     ##TODO: Install missing required packages
     ##TODO: Include required packages
     
-    ## Import data file
-    if (!exists("shelter.data")){
-        cat("Please select the shelter data csv file.\n")
-        shelter.data <- read.csv(file.choose(), header=T, sep=";")
-        assign("shelter.data", shelter.data, envir = .GlobalEnv)
+    ## Import data files
+    if (!exists("n.items")){
+        cat("Please select the shelter_number data csv file.\n")
+        n.items <- read.csv(file.choose(), header=T, sep=";")
+        assign("n.items", n.items, envir = .GlobalEnv)
+    }
+    if (!exists("survival.time")){
+      cat("Please select the shelter_survival data csv file.\n")
+      survival.time <- read.csv(file.choose(), header=T, sep=";")
+      survival.time <- subset(survival.time, select = -c(name))
+      assign("survival.time", survival.time, envir = .GlobalEnv)
     }
 }
 
 ##TODO Examine importance of features (even if you will not discard any).
 examine <- function(){
   cat("Number of items vs. time, dmg and SPECIAL stats:\n")
-  print(summary(lm(n~.,shelter.data))$coefficients)
+  print(summary(lm(n~.,n.items))$coefficients)
   cat("\n\nNumber of items/h vs. dmg and SPECIAL stats:\n")
-  print(summary(lm(n/time~.,shelter.data))$coefficients)
+  print(summary(lm(n/time~.,n.items))$coefficients)
+  cat("\n\nSurvival time vs. dmg and SPECIAL stats")
+  print(summary(lm(survival.time~.,survival.time))$coefficients)
+  cat("\n\nCaps/hour vs. dmg and SPECIAL stats")
+  print(summary(lm(caps/survival.time~.,survival.time))$coefficients)
 }
 
 ##TODO If too many features:
